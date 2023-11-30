bce597738d215ceecdcc9670d47e7a16f0ee2ac3
JohnIsutsa <37325810+JohnIsutsa@users.noreply.github.com>
2023-11-30 02:18:22 +0000

    Added game logic

---
+++ app.py
@@ -0,0 +1,60 @@
+import random
+
+def get_user_choice():
+    """Get the user's choice."""
+    while True:
+        user_choice = input("Choose rock, paper, or scissors: ").lower()
+        if user_choice in ["rock", "paper", "scissors"]:
+            return user_choice
+        else:
+            print("Invalid choice. Please enter rock, paper, or scissors.")
+
+def get_computer_choice():
+    """Generate the computer's choice."""
+    choices = ["rock", "paper", "scissors"]
+    return random.choice(choices)
+
+def determine_winner(user_choice, computer_choice):
+    """Determine the winner of the game."""
+    if user_choice == computer_choice:
+        return "It's a tie!"
+    elif (
+        (user_choice == "rock" and computer_choice == "scissors") or
+        (user_choice == "paper" and computer_choice == "rock") or
+        (user_choice == "scissors" and computer_choice == "paper")
+    ):
+        return "You win!"
+    else:
+        return "Computer wins!"
+
+def play_game():
+    """Play the Rock, Paper, Scissors game."""
+    print("Welcome to Rock, Paper, Scissors!")
+
+    user_score = 0
+    computer_score = 0
+
+    while True:
+        user_choice = get_user_choice()
+        computer_choice = get_computer_choice()
+
+        print(f"\nYou chose: {user_choice}")
+        print(f"Computer chose: {computer_choice}")
+
+        result = determine_winner(user_choice, computer_choice)
+        print(result)
+
+        if "You win" in result:
+            user_score += 1
+        elif "Computer wins" in result:
+            computer_score += 1
+
+        print(f"\nCurrent Score - You: {user_score} | Computer: {computer_score}")
+
+        play_again = input("Do you want to play again? (yes/no): ").lower()
+        if play_again != "yes":
+            print("Thanks for playing. Final Score - You: {} | Computer: {}".format(user_score, computer_score))
+            break
+
+# Start the game
+play_game()
